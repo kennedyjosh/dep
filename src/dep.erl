402abd2c900be192bbfd750d9555d6e9b26268fe
kennedyjosh <joshua@halloapp.com>
2021-08-27 14:18:28 -0700

    Fixes

--- src/dep.erl
+++ src/dep.erl
@@ -1,8 +1,15 @@
 -module(dep).
 
 %% API
--export([test/0]).
+-export([test/0, use_proto/0, use_hdr/0]).
 
+-include("noise_server.hrl").
 -include("dep.hrl").
 
 test() -> ?OK.
+
+use_proto() ->
+    noise_server:get_all_proto_names().
+
+use_hdr() ->
+    #pb_noise_message{message_type = 'XX_A', content = <<>>}.
--- test/dep_tests.erl
+++ test/dep_tests.erl
@@ -3,4 +3,11 @@
 -include_lib("eunit/include/eunit.hrl").
 
 simple_test() ->
+    ?assert(length(dep:use_proto()) > 0).
+
+simpler_test() ->
     ?assertEqual(ok, dep:test()).
+
+use_hdr_test() ->
+    {pb_noise_message, _, _} = dep:use_hdr().
+
